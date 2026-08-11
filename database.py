57dd9b93fc64420a6f615364b2b4f5cde5f0da05
sanachougule50-tech <sanachougule50@gmail.com>
2026-08-11 10:00:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -61,6 +61,50 @@ def create_database():
     )
     """)
 
+    cursor.execute("DELETE FROM teachers")
+    cursor.execute("DELETE FROM assignments")
+
+    clean_teachers = [
+        ("Varsha Shinde", "varsha@sies.edu", "Data Science"),
+        ("Rashmi Prabha", "rashmi@sies.edu", "Data Science"),
+        ("Tina Tommy", "tina@sies.edu", "Data Science"),
+        ("Palak Jadhav", "palak@sies.edu", "Data Science"),
+        ("Sana Chougule", "sana@sies.edu", "Data Science"),
+        ("Anita Desai", "anita@sies.edu", "Data Science"),
+        ("Ramesh Iyer", "ramesh@sies.edu", "Data Science"),
+        ("Priya Nair", "priya@sies.edu", "Data Science"),
+        ("Suresh Patil", "suresh@sies.edu", "Data Science"),
+        ("Vishal Kumar", "vishal@sies.edu", "Data Science"),
+        ("Nutan Sawant", "nutan@sies.edu", "Data Science")
+    ]
+    for name, email, dept in clean_teachers:
+        cursor.execute(
+            "INSERT INTO teachers (teacher_name, email, department) VALUES (?, ?, ?)",
+            (name, email, dept)
+        )
+
+    assignments = [
+        ("FY DS", "FDS", "Rashmi Prabha", "CR-02"),
+        ("FY DS", "Python Programming", "Nutan Sawant", "CR-02"),
+        ("FY DS", "Descriptive Statistics", "Varsha Shinde", "CR-02"),
+        ("FY DS", "SIES Development", "Tina Tommy", "CR-02"),
+
+        ("SY DS", "DSA", "Sana Chougule", "CR-03"),
+        ("SY DS", "Statistical Inference", "Anita Desai", "CR-03"),
+        ("SY DS", "CC - SIES Development", "Ramesh Iyer", "CR-03"),
+        ("SY DS", "OE1 - Social Media Marketing", "Priya Nair", "CR-03"),
+
+        ("TY DS", "Big Data Analytics", "Suresh Patil", "CR-04"),
+        ("TY DS", "Deep Learning", "Vishal Kumar", "CR-04"),
+        ("TY DS", "Natural Language Processing (NLP)", "Rashmi Prabha", "CR-04"),
+        ("TY DS", "Research Methodology in Data Science", "Palak Jadhav", "CR-04"),
+    ]
+    for class_name, subject_name, teacher_name, room_name in assignments:
+        cursor.execute("""
+        INSERT INTO assignments (class_name, subject_name, teacher_name, room_name)
+        VALUES (?, ?, ?, ?)
+        """, (class_name, subject_name, teacher_name, room_name))
+
     conn.commit()
     conn.close()
 
